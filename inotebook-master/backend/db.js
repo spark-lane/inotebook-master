e698d0040907fa15b87359c920b4b3e6a60bd1e8
spark-lane <omsore47@gmail.com>
2023-06-06 22:37:22 +0530

    initial commit

---
+++ inotebook-master/backend/db.js
@@ -0,0 +1,12 @@
+const mongoose =require('mongoose');
+
+
+
+const mongoURI = "mongodb://127.0.0.1:27017/inotebook?directConnection=true";
+
+const connectToMongo=()=>{
+    mongoose.connect(mongoURI,()=>{
+        console.log("Connect to Mongo Successfully");
+    })
+}
+module.exports=connectToMongo;
